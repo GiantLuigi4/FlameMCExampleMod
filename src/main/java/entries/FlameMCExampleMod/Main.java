afe10d172dd719b03f915e5fa76a7996679f6dd1
GiantLuigi4 <tjmferet@hotmail.com>
2020-08-21 16:12:06 -0400

    Block Item registry example.

--- src/main/java/entries/FlameMCExampleMod/Main.java
+++ src/main/java/entries/FlameMCExampleMod/Main.java
@@ -3,6 +3,8 @@ package entries.FlameMCExampleMod;
 import com.tfc.API.flamemc.Registry;
 import com.tfc.API.flamemc.blocks.BlockInstancer;
 import com.tfc.API.flamemc.blocks.BlockPropeteries;
+import com.tfc.API.flamemc.items.BlockItem;
+import com.tfc.API.flamemc.items.ItemProperties;
 import com.tfc.flame.IFlameMod;
 
 public class Main implements IFlameMod {
@@ -19,6 +21,8 @@ public class Main implements IFlameMod {
 		//The name of the block
 		Registry.ResourceLocation location = new Registry.ResourceLocation("flame_example_mod:test");
 		//Register a generic block
-		Registry.register(location, Registry.RegistryType.BLOCK, BlockInstancer.instanceBlock(new BlockPropeteries(location, Registry.get(Registry.RegistryType.BLOCK, new Registry.ResourceLocation("minecraft:stone")))));
+		Registry.RegistryObject<?> block = Registry.register(location, Registry.RegistryType.BLOCK, BlockInstancer.instanceBlock(new BlockPropeteries(location, Registry.get(Registry.RegistryType.BLOCK, new Registry.ResourceLocation("minecraft:stone")))));
+		//Register an item for the generic block
+		Registry.registerBlockItem(location,block);
 	}
 }
