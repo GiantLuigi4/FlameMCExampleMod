98765978ef9ec99addcd247ea142d35ad37b69a2
GiantLuigi4 <tjmferet@hotmail.com>
2020-08-21 22:18:24 -0400

    Example Item

--- src/main/java/entries/FlameMCExampleMod/Main.java
+++ src/main/java/entries/FlameMCExampleMod/Main.java
@@ -1,10 +1,13 @@
 package entries.FlameMCExampleMod;
 
 import com.tfc.API.flame.FlameAPI;
+import com.tfc.API.flame.utils.logging.Logger;
 import com.tfc.API.flamemc.event.init_steps.RegistryStep;
 import com.tfc.API.flamemc.Registry;
 import com.tfc.API.flamemc.blocks.BlockInstancer;
 import com.tfc.API.flamemc.blocks.BlockPropeteries;
+import com.tfc.API.flamemc.items.Item;
+import com.tfc.API.flamemc.items.ItemProperties;
 import com.tfc.flame.IFlameMod;
 
 public class Main implements IFlameMod {
@@ -28,5 +31,9 @@ public class Main implements IFlameMod {
 		Registry.RegistryObject<?> block = Registry.register(location, Registry.RegistryType.BLOCK, BlockInstancer.instanceBlock(new BlockPropeteries(location, Registry.get(Registry.RegistryType.BLOCK, new Registry.ResourceLocation("minecraft:stone")))));
 		//Register an item for the generic block
 		Registry.registerBlockItem(location,block);
+		//The name of the item
+		location = new Registry.ResourceLocation("flame_example_mod:test_item");
+		//Register a generic item
+		Registry.register(location, Registry.RegistryType.ITEM, Item.instance(new ItemProperties(location,Registry.get(Registry.RegistryType.ITEM,new Registry.ResourceLocation("minecraft:diamond_sword")))));
 	}
 }
